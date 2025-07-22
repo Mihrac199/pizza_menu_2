0087c28dd3dfb972eeb81e0608d61472e2501620
Mihrac199 <mihrac.comart1996@gmail.com>
2025-07-22 15:49:57 +0300

    Update

---
+++ src/ui/Home.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+
+  return (
+
+    <div>
+
+      <h1>
+        The best pizza.
+        <br />
+        Straight out of the oven, straight to you.
+      </h1>
+
+    </div>
+
+  )
+
+}
