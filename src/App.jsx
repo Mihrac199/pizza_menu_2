4db094cf1e9c6db325084a31812f348d2a8f80a7
Mihrac199 <mihrac.comart1996@gmail.com>
2025-07-24 14:55:25 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+import CreateOrder from "./features/order/CreateOrder"
+import Order from "./features/order/Order"
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "menu",
+    element: <Menu />
+  },
+  {
+    path: "cart",
+    element: <Cart />
+  },
+  {
+    path: "order/new",
+    element: <CreateOrder />
+  },
+  {
+    path: "order/:orderId",
+    element: <Order />
+  }
+
+])
+
 export default function App() {
-
-  const vite = "Hello Vite + React";
-
-  return (
-
-    <div>
-      <h1>{vite}</h1>
-    </div>
-
-  )
-
+  return <RouterProvider router={router} />
 }
